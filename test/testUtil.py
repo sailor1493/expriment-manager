288224e9bf5cefbb160e636e30b2d7f3368cf674
Chanwoo Park <99chanwoo@gmail.com>
2023-01-11 12:41:02 +0900

    test: add override parameter to test function

--- test/testUtil.py
+++ test/testUtil.py
@@ -6,6 +6,8 @@ Testing module for experiment.util module
 
 import unittest
 from experiment.util import compare_object_dict
+from collections import namedtuple
+from typing import Optional, List
 
 
 class TestUtil(unittest.TestCase):
@@ -18,8 +20,14 @@ class TestUtil(unittest.TestCase):
         right = 1
         self.__test_compare(left, right, False)
 
-    def __test_compare(self, left: object, right: object, expected: bool):
-        self.assertEqual(compare_object_dict(left, right), expected)
+    def __test_compare(
+        self,
+        left: object,
+        right: object,
+        expected: bool,
+        override: Optional[List[str]] = None,
+    ):
+        self.assertEqual(compare_object_dict(left, right, override), expected)
 
 
 if __name__ == "__main__":
